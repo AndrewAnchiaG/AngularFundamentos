251eecd1cafcb1324ec692d23173582f620162dd
Andrew O. Anchía González <andrew.anchia@deoper.com>
2023-11-11 15:29:56 -0600

    refactor(list): Directiva NgIf, NgFor, Ng-template

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -3,9 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-heroes-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-  public heroesNames:string[] = ["Dr. Strange", "Tork", "Ironman", "Hulk", "Ant-Man", "BlackWidow", "Hela","Suri", "Black Panter", "Nebula","Cap. Marvel", "Bruja Escarlata","Valquiria", "Sersi", "Okoye"];
+  public heroesNames: string[] = [
+    'Dr. Strange',
+    'Tork',
+    'Ironman',
+    'Hulk',
+    'Ant-Man',
+    'BlackWidow',
+    'Hela',
+    'Suri',
+    'Black Panter',
+    'Nebula',
+    'Cap. Marvel',
+    'Bruja Escarlata',
+    'Valquiria',
+    'Sersi',
+    'Okoye',
+  ];
+  public heroeliminated: string | undefined;
+  public starDelete: boolean = false;
 
+  public removeLastHeroe(): void {
+    this.starDelete = true;
+    this.heroeliminated = this.heroesNames.pop();
+  }
 }
