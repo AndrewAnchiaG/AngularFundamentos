195cb43ecf20ff18331c3d252d0bc607d823d59a
Andrew O. Anchía González <andrew.anchia@deoper.com>
2023-11-11 14:07:52 -0600

    refactor(hero): use interpolation, pipe, capitalize

---
+++ src/app/heroes/hero/hero.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero',
+  templateUrl: './hero.component.html',
+  styleUrls: ['./hero.component.css']
+})
+
+export class HeroComponent {
+  public name: string = "dr. strange"
+  public age: number = 45;
+
+  get capitalizedName():string {
+    return this.name.toUpperCase();
+  }
+
+  public getHeroDescription():string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  public changeHero(): void{
+    this.name = "Spiderman"
+  }
+
+  public changeAge(): void{
+    this.age = 25;
+  }
+}
