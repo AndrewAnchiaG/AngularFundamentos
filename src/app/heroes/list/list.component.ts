3190508290e2249cc562ba28f608dcaf8d6c3639
Andrew O. Anchía González <andrew.anchia@deoper.com>
2023-11-11 14:47:33 -0600

    feat(list): directiva *NgFor

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent {
+  public heroesNames:string[] = ["Dr. Strange", "Tork", "Ironman", "Hulk", "Ant-Man", "BlackWidow", "Hela","Suri", "Black Panter", "Nebula","Cap. Marvel", "Bruja Escarlata","Valquiria", "Sersi", "Okoye"];
 
 }
