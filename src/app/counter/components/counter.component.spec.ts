4d7f842f12890215a674b15713238d0756860a61
Andrew O. Anchía González <andrew.anchia@deoper.com>
2023-11-11 16:36:30 -0600

    perf(heroModule,listModule):split module

--- src/app/counter/components/counter.component.spec.ts
+++ src/app/counter/components/counter.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CounterComponent } from './counter.component';
+import { CounterComponent } from '../counter.component';
 
 describe('CounterComponent', () => {
   let component: CounterComponent;
