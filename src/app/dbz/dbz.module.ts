a7a1e945e5ad5dc64e88e09e35c398f18e9c66c2
Andrew O. Anchía González <andrew.anchia@deoper.com>
2023-11-11 19:15:54 -0600

    perf(add-character | list): split main component

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DbzMainPageComponent } from './pages/dbz-main-page/dbz-main-page.component';
+import { ListComponent } from './components/list/list.component';
+import { AddCharacterComponent } from './components/add-character/add-character.component';
 
 
 
 @NgModule({
   declarations: [
-    DbzMainPageComponent
+    DbzMainPageComponent,
+    ListComponent,
+    AddCharacterComponent
   ],
   imports: [
     CommonModule
