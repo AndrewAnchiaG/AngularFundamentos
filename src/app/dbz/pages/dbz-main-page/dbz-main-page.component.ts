c71a8a781f642b8d040f3ac8a8e197d7b14b4d6a
Andrew O. Anchía González <andrew.anchia@deoper.com>
2023-11-11 18:32:01 -0600

    feat(dbz-main-page): add this component

---
+++ src/app/dbz/pages/dbz-main-page/dbz-main-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dbz-main-page',
+  templateUrl: './dbz-main-page.component.html',
+  styleUrls: ['./dbz-main-page.component.css']
+})
+export class DbzMainPageComponent {
+
+}
